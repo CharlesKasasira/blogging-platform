37278c42f4f5cc6955657d5b207c19356a97fc98
CharlesKasasira <charleskasasira01@gmail.com>
2021-10-21 11:52:18 +0300

    adding cors

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8889
 const app = express();
+
+// app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  
+    allowedHeaders: ['Content-Type']
+}))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"))
 })
 
-app.get("/todos", (req, res) => {
+app.get("/todos", cors(), (req, res) => {
     res.sendFile(path.join(__dirname, "json", "todos.json"))
 })
 
 app.get("/posts", (req, res) => {
     res.sendFile(path.join(__dirname, "json", "posts.json"))
 })
+
+console.log(cors)
 
 
 
